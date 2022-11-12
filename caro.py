dac876c1bc6c2343c3605bbe2a9d3b40325d5d15
katashirintarou <108202070+AranAyumu@users.noreply.github.com>
2022-11-12 19:46:49 +0700

    Update caro.py

--- caro.py
+++ caro.py
@@ -1,31 +1,31 @@
-mapslot= [' ',' ',' ',' ',' ',' ',' ',' ',' ','5']
+map_slot= [' ',' ',' ',' ',' ',' ',' ',' ',' ']
 
 turn = 0
 last_turn = ''
 while turn > -1:
     while turn > -1:
         for x in range(3):
-            print(mapslot[x*3] + "|" + mapslot[x*3+1] + "|" + mapslot[x*3+2])
+            print(map_slot[x*3] + "|" + map_slot[x*3+1] + "|" + map_slot[x*3+2])
         location = input()  
-        if(mapslot[int(location)-1] == ' '):
+        if(map_slot[int(location)-1] == ' '):
             if(turn % 2 == 0):
-                mapslot[int(location)-1] = 'x'
+                map_slot[int(location)-1] = 'x'
                 turn += 1
-                last_turn = mapslot[int(location)-1]
+                last_turn = map_slot[int(location)-1]
                 break
             
             else:
-                mapslot[int(location)-1] = 'o'    
+                map_slot[int(location)-1] = 'o'    
                 turn += 1
-                last_turn = mapslot[int(location)-1]
+                last_turn = map_slot[int(location)-1]
                 break
-    if(mapslot[0] != ' ' and mapslot[0] == mapslot[1] and mapslot[0] == mapslot[2] or mapslot[3] != ' ' and mapslot[3] == mapslot[4] and mapslot[3] == mapslot[5] or mapslot[6] != ' ' and mapslot[6] == mapslot[7] and mapslot[6] == mapslot[8] or mapslot[0] != ' ' and mapslot[0] == mapslot[3] and mapslot[0] == mapslot[6] or mapslot[1] != ' ' and mapslot[1] == mapslot[4] and mapslot[1] == mapslot[7] or mapslot[2] != ' ' and mapslot[2] == mapslot[5] and mapslot[2] == mapslot[8] or mapslot[0] != ' ' and mapslot[0] == mapslot[4] and mapslot[0] == mapslot[8] or mapslot[2] != ' ' and mapslot[2] == mapslot[4] and mapslot[2] == mapslot[6]):
+    if(map_slot[0] != ' ' and map_slot[0] == map_slot[1] and map_slot[0] == map_slot[2] or map_slot[3] != ' ' and map_slot[3] == map_slot[4] and map_slot[3] == map_slot[5] or map_slot[6] != ' ' and map_slot[6] == map_slot[7] and map_slot[6] == map_slot[8] or map_slot[0] != ' ' and map_slot[0] == map_slot[3] and map_slot[0] == map_slot[6] or map_slot[1] != ' ' and map_slot[1] == map_slot[4] and map_slot[1] == map_slot[7] or map_slot[2] != ' ' and map_slot[2] == map_slot[5] and map_slot[2] == map_slot[8] or map_slot[0] != ' ' and map_slot[0] == map_slot[4] and map_slot[0] == map_slot[8] or map_slot[2] != ' ' and map_slot[2] == map_slot[4] and map_slot[2] == map_slot[6]):
       for x in range(3):
-             print(mapslot[x*3] + "|" + mapslot[x*3+1] + "|" + mapslot[x*3+2])
+             print(map_slot[x*3] + "|" + map_slot[x*3+1] + "|" + map_slot[x*3+2])
       print("the winner is: " + last_turn)  
       break
-    elif(mapslot[0] != ' ' and mapslot[1] != ' ' and mapslot[2] != ' ' and mapslot[3] != ' ' and mapslot[4] != ' ' and mapslot[5] != ' ' and mapslot[6] != ' ' and mapslot[7] != ' ' and mapslot[8] != ' '):
+    elif(map_slot[0] != ' ' and map_slot[1] != ' ' and map_slot[2] != ' ' and map_slot[3] != ' ' and map_slot[4] != ' ' and map_slot[5] != ' ' and map_slot[6] != ' ' and map_slot[7] != ' ' and map_slot[8] != ' '):
     	 for x in range(3):
-             print(mapslot[x*3] + "|" + mapslot[x*3+1] + "|" + mapslot[x*3+2])
+             print(map_slot[x*3] + "|" + map_slot[x*3+1] + "|" + map_slot[x*3+2])
     	 print("match tie")
     	 break
